038a14dde906a1fc258d05447921c85f2f160974
nabamjoss6321@gmail.com <nabamjoss6321@gmail.com>
2021-01-14 19:41:49 +0530

    Fibonnaci Number

---
+++ Excrise 13.py
@@ -0,0 +1,17 @@
+def fibonnaci():
+    num = int(input("How many fibonnaci number do u want: "))
+    i = 1
+    if num == 0:
+        fib = []
+    elif num == 1:
+        fib = [1]
+    elif num == 2:
+        fib = [1,1]
+    elif num > 2:
+        fib = [1,1]
+        while i < (num - 1):
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+    return fib
+
+print(fibonnaci())
