5a49f218f3acebc386e557d24e2fc3660dd30d5d
nabamjoss6321@gmail.com <nabamjoss6321@gmail.com>
2021-01-14 18:18:57 +0530

    Prime Number

--- Excrise 11.py
+++ Excrise 11.py
@@ -1,1 +1,14 @@
-print("hello world!")
+num = int(input("Write a Number: "))
+
+a = [i for i in range(2, num) if num % i == 0]
+
+def is_prime(x):
+    if num > 1:
+        if len(a) == 0:
+            print("It's a prime number")
+        else:
+            print("It's not a prime number")
+    else:
+        print("It's not a prime number")
+
+is_prime(num)
