8c94140ff83cc7b7392a0cd468af4b64cf3042f0
nabamjoss6321@gmail.com <nabamjoss6321@gmail.com>
2021-01-20 14:55:55 +0530

    Decode A Web Page Two

--- Excrise 19.py
+++ Excrise 19.py
@@ -4,10 +4,6 @@ from bs4 import BeautifulSoup
 url = requests.get("https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture")
 html = url.text
 page = BeautifulSoup(html, 'html.parser')
-match = page.find('div', {'class': 'article__chunks'})
 
-page_list = [[k.get_text() for k in i.find_all('p')] for i in match]
 
-for i in page_list[:-2]:
-   for k in i:
-     print(k + '\n')
+print(*[p.get_text() for p in page.select('article.article.main-content p')], sep = '\n')
